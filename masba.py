77226f32b4df7b62733ca75d688818b9a77d5464
MasbaIUBAT <masba_rumi@yahoo.com>
2020-06-28 23:07:43 +0600

    just for practice

---
+++ masba.py
@@ -0,0 +1,24 @@
+class Employee:
+    increment = 1.5
+    def __init__(self,fname,lname,salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+        self.increment = 1.9
+    def increase(self):
+        self.salary = self.salary * Employee.increment
+    @classmethod
+    def change_increment(cls,amount):
+        cls.increment = amount
+
+    @staticmethod
+    def isopen(day):
+        if day == 'sunday':
+            return False 
+        else:
+            return True
+#Raghib_Shahriar = Employee('harry','jack',4000)
+print(Employee.isopen('sunday'))
+
+
+
